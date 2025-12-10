90a3c92ef121553878d2768f62cb2ba425cc2a64
Matthieu Pignolet <matthieu@puffer.fish>
2025-12-10 15:22:14 +0100

    feat: add password field to users

--- schema.sql
+++ schema.sql
@@ -20,6 +20,7 @@ CREATE TABLE public.users (
 	username varchar NOT NULL,
 	display_name varchar NOT NULL,
 	email varchar NOT NULL,
+    password varchar NOT NULL,
 	CONSTRAINT users_pk PRIMARY KEY (username)
 );
 
