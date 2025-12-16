6ed0a22d3f703b0ce8568d17f2bde778a290d1a5
Matthieu Pignolet <matthieu@puffer.fish>
2025-12-16 12:32:18 +0100

    feat: ajout des définitions d'api pour les posts

--- back/api/Post.py
+++ back/api/Post.py
@@ -1,4 +1,41 @@
-from flask_openapi3 import APIBlueprint
-from structures import Post
+from flask_openapi3 import APIBlueprint, Tag
+from structures import Post, PostList, PostSubmit, GetPostQuery
 
-api = APIBlueprint("Post", __name__, url_prefix="/post")
+api = APIBlueprint("Post", __name__, url_prefix="/posts")
+
+post_tag = Tag(name="Post", description="Operations related to posts")
+
+@api.get("/",
+        tags=[post_tag],
+        responses={200: PostList}
+)
+def get_posts() -> PostList:
+    raise NotImplementedError("Post retrieval not implemented yet.")
+
+
+@api.get("/<uuid>",
+        tags=[post_tag],
+        responses={200: Post}
+)
+def get_post_by_id(
+    uuid: str,
+    query: GetPostQuery
+) -> Post:
+    raise NotImplementedError("Post retrieval by ID not implemented yet.")
+
+@api.post("/",
+          tags=[post_tag],
+          responses={201: Post},
+)
+def create_post(
+    body: PostSubmit
+) -> Post:
+    raise NotImplementedError("Post creation not implemented yet.")
+
+@api.delete("/<uuid>",
+        tags=[post_tag],
+        responses={204: None}
+)
+def del_post(uuid: str) -> None:
+    raise NotImplementedError("Post deletion not implemented yet.")
+
--- back/structures/Post.py
+++ back/structures/Post.py
@@ -1,9 +1,31 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, RootModel
 from structures.User import User
+from typing import List, Optional
 
 class Post(BaseModel):
     title: str
     content: str
     author: User
-    children: list[Post]
+    response_to: Optional[str] = None 
+    replies: List[Post] = []
 
+class PostSubmit(BaseModel):
+    title: str
+    content: str
+    response_to: Optional[str] = None
+
+class PostUpdate(BaseModel):
+    title: Optional[str] = None
+    content: Optional[str] = None
+
+class PostList(RootModel):
+    root: List[Post]
+
+    def __iter__(self):
+        return iter(self.root)
+
+    def __getitem__(self, item):
+        return self.root[item]
+    
+class GetPostQuery(BaseModel):
+    depth: int = 0
