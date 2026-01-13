b08691639d8a023b1c554e40d2325bd49fba7bcc
Tarek <tarek.belbachir@telecomnancy.eu>
2026-01-13 13:39:40 +0100

    creation d'une api pour upload et telechargement de fichiers

--- back/api/tags.py
+++ back/api/tags.py
@@ -3,3 +3,7 @@ from flask_openapi3 import Tag
 user_tag = Tag(name="User", description="Operations related to users")
 reaction_tag = Tag(name="Reaction", description="Operations related to reactions")
 post_tag = Tag(name="Post", description="Operations related to posts")
+attachment_tag = {
+    "name": "attachments",
+    "description": "File upload and download"
+}
